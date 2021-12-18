7ae8806a17341fdf9eb9696d8d1688af40c1eaa7
Tamas Ruszka <ruszka.tamas@gmail.com>
2021-12-18 19:34:49 +0100

    Day 1 - Part 2 solution

--- src/main/java/hu/tamasruszka/adventofcode/Day01.java
+++ src/main/java/hu/tamasruszka/adventofcode/Day01.java
@@ -20,6 +20,7 @@ public class Day01 {
 
 	public static void main(String[] args) throws IOException {
 		part1();
+		part2();
 	}
 
 	private static void part1() throws IOException {
@@ -28,17 +29,46 @@ public class Day01 {
 											   .mapToInt(Integer::parseInt)
 											   .boxed()
 											   .collect(Collectors.toList());
-		Integer increasedCount = 0;
+		int increasedCount = 0;
 
 		for (int i = 0; i < depthValues.size() - 1; i++) {
-			Integer currentDepth = depthValues.get(i);
-			Integer nextDepth = depthValues.get(i + 1);
+			int currentDepth = depthValues.get(i);
+			int nextDepth = depthValues.get(i + 1);
 
 			if (nextDepth > currentDepth) {
 				increasedCount++;
 			}
 		}
 
+		log.info("Part 1 solution");
+		log.info("Depth increased {} times", increasedCount);
+	}
+
+	private static void part2() throws IOException {
+		List<String> sourceLines = readSourceFile();
+		List<Integer> depthValues = sourceLines.stream()
+											   .mapToInt(Integer::parseInt)
+											   .boxed()
+											   .collect(Collectors.toList());
+		int increasedCount = 0;
+
+		for (int i = 0; i < depthValues.size() - 3; i++) {
+			int currentDepth1 = depthValues.get(i);
+			int currentDepth2 = depthValues.get(i + 1);
+			int currentDepth3 = depthValues.get(i + 2);
+			int nextDepth1 = depthValues.get(i + 1);
+			int nextDepth2 = depthValues.get(i + 2);
+			int nextDepth3 = depthValues.get(i + 3);
+
+			int currentDepthAggregated = currentDepth1 + currentDepth2 + currentDepth3;
+			int nextDepthAggregated = nextDepth1 + nextDepth2 + nextDepth3;
+
+			if (nextDepthAggregated > currentDepthAggregated) {
+				increasedCount++;
+			}
+		}
+
+		log.info("Part 2 solution");
 		log.info("Depth increased {} times", increasedCount);
 	}
 
